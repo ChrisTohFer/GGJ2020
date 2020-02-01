55f600f50c1951338ed2ff5327569cb1a833a6b9
Chris <chrisrkemp@hotmail.co.uk>
2020-02-01 10:13:36 +0000

    Added initial file input for language lists

---
+++ Assets/Game/C#/Non-Monobehaviour/FileLoader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+class GAME_IO_FUNCTIONS
+{
+    public static string[] ReadFile(string path)
+    {
+        Debug.Log(Directory.GetCurrentDirectory());
+
+        if(!File.Exists(path))
+        {
+            Debug.Log("Could not find file at path: " + path);
+            return null;
+        }
+
+        return File.ReadAllLines(path);
+    }
+
+    public static string[] ReadNounList()
+    {
+        return ReadFile("configs/NounList.txt");
+    }
+
+    public static string[] ReadVerbList()
+    {
+        return ReadFile("configs/VerbList.txt");
+    }
+}
