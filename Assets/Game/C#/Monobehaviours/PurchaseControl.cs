58f2a8cdd82eeb8276a71c502d6f2617e02dc8ad
Chris <chrisrkemp@hotmail.co.uk>
2020-02-01 18:33:48 +0000

    Changes to shop prefab and added rotators to scene

--- Assets/Game/C#/Monobehaviours/PurchaseControl.cs
+++ Assets/Game/C#/Monobehaviours/PurchaseControl.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class PurchaseControl : MonoBehaviour
 {
@@ -9,11 +10,25 @@ public class PurchaseControl : MonoBehaviour
     public UnityEvent Purchase;
     [SerializeField] int cost;
 
+    //
+    Button purchaseButton;
+
+    void Awake()
+    {
+        purchaseButton = GetComponent<Button>();
+        purchaseButton.onClick.AddListener(ButtonPressed);
+    }
+    void OnDestroy()
+    {
+        purchaseButton.onClick.RemoveListener(ButtonPressed);
+    }
+
     public void ButtonPressed()
     {
         if(MoneyManager.Decrease(cost))
         {
             Purchase.Invoke();
+            purchaseButton.interactable = false;
         }
     }
 }
---
+++ Assets/Game/C#/Monobehaviours/Rotator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    //
+    [SerializeField] float RadiansPerUnit = 1f;
+
+    //
+    bool Rotating = false;
+
+    void Update()
+    {
+        if(Input.GetMouseButtonUp(0))
+        {
+            Rotating = false;
+        }
+        if(Rotating)
+        {
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x
+                                            , transform.eulerAngles.y - RadiansPerUnit * Input.GetAxis("Mouse X")
+                                            , transform.eulerAngles.z);
+        }
+    }
+
+    void OnMouseDown()
+    {
+        Rotating = true;
+    }
+
+}
