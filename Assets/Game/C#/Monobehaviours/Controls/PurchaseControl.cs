ca6cdc6c8a867b0977c8da65e01d89f7e187f22b
Chris <chrisrkemp@hotmail.co.uk>
2020-02-02 15:43:54 +0000

    Final chnages

--- Assets/Game/C#/Monobehaviours/Controls/PurchaseControl.cs
+++ Assets/Game/C#/Monobehaviours/Controls/PurchaseControl.cs
@@ -10,21 +10,31 @@ public class PurchaseControl : MonoBehaviour
     public UnityEvent Purchase;
     [SerializeField] int cost;
     [SerializeField] int id;
+    [SerializeField] GameObject SendItButton;
 
     bool purchased = false;
     bool bookOpen = false;
+
+    UnityEvent openbook = new UnityEvent();
 
     //
     Button purchaseButton;
 
     void Awake()
     {
+        openbook.AddListener(SetOpenFalse);
         purchaseButton = GetComponent<Button>();
         purchaseButton.onClick.AddListener(ButtonPressed);
     }
     void OnDestroy()
     {
+        openbook.RemoveListener(SetOpenFalse);
         purchaseButton.onClick.RemoveListener(ButtonPressed);
+    }
+
+    void SetOpenFalse()
+    {
+        bookOpen = false;
     }
 
     public void ButtonPressed()
@@ -35,23 +45,36 @@ public class PurchaseControl : MonoBehaviour
             {
                 Purchase.Invoke();
                 purchased = true;
+                OpenBook();
             }
         }
         else
         {
             if(bookOpen)
             {
-                TextBoxController.ReturnMainText();
-                bookOpen = false;
+                CloseBook();
             }
             else
             {
-                string componentString = LanguageManager.Singleton.components.GetAlienByIndex(id) + " = " + LanguageManager.Singleton.components.GetTypeByIndex(id) + " component";
-                string socketString = LanguageManager.Singleton.sockets.GetAlienByIndex(id) + " = " + LanguageManager.Singleton.sockets.GetTypeByIndex(id) + " socket";
-
-                TextBoxController.ChangeText(componentString + "\n" + socketString, false);
-                bookOpen = true;
+                OpenBook();
             }
         }
+    }
+
+    void OpenBook()
+    {
+        openbook.Invoke();
+        string componentString = LanguageManager.Singleton.components.GetAlienByIndex(id) + " = " + LanguageManager.Singleton.components.GetTypeByIndex(id) + " component";
+        string socketString = LanguageManager.Singleton.sockets.GetAlienByIndex(id) + " = " + LanguageManager.Singleton.sockets.GetTypeByIndex(id) + " socket";
+
+        TextBoxController.ChangeText(componentString + "\n" + socketString, false);
+        bookOpen = true;
+        SendItButton.SetActive(false);
+    }
+    void CloseBook()
+    {
+        TextBoxController.ReturnMainText();
+        bookOpen = false;
+        SendItButton.SetActive(true);
     }
 }
