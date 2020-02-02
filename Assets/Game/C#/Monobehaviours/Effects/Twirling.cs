a9f93c12ed32b5678b3e078d359244d3e9338c92
Chris <chrisrkemp@hotmail.co.uk>
2020-02-02 10:59:25 +0000

    Added twirling

---
+++ Assets/Game/C#/Monobehaviours/Effects/Twirling.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Twirling : MonoBehaviour
+{
+    [SerializeField] Vector3 initialRotation;   //0 to 2pi, incremented each frame depending on period
+    [SerializeField] Vector3 rotationPeriod;
+    [SerializeField] Vector3 rotationScale;
+
+    void Update()
+    {
+        initialRotation.x = Mathf.Repeat(initialRotation.x + (Time.deltaTime / rotationPeriod.x) * 2f * Mathf.PI, 2f * Mathf.PI);
+        initialRotation.y = Mathf.Repeat(initialRotation.y + (Time.deltaTime / rotationPeriod.y) * 2f * Mathf.PI, 2f * Mathf.PI);
+        initialRotation.z = Mathf.Repeat(initialRotation.z + (Time.deltaTime / rotationPeriod.z) * 2f * Mathf.PI, 2f * Mathf.PI);
+        transform.eulerAngles = 180f / Mathf.PI * new Vector3(
+                                    Mathf.Sin(initialRotation.x) * rotationScale.x,
+                                    Mathf.Sin(initialRotation.y) * rotationScale.y,
+                                    Mathf.Sin(initialRotation.z) * rotationScale.z);
+    }
+}
+        
